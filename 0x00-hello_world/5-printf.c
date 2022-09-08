a6a510c580b705c6dbb0db9ee2a4434769902a20
erokemwa <erokemwa@yahoo.com>
2022-09-08 12:32:45 -0700

    Print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcomeis a piece of art,\n");
+return (0);
+}
