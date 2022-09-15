701979b13b6b231491e1bfeca675ac8873330394
erokemwa <erokemwa@yahoo.com>
2022-09-15 08:58:39 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Prints lines using_
+ * @n: number of _
+ */
+
+void print_line(int n)
+{
+	 int a;
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+
+}
+else
+_putchar('\n');
+
+}
+
+
