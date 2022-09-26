a90108a137f70383b128b98e60839216dc760fb0
erokemwa <erokemwa@yahoo.com>
2022-09-26 08:51:49 -0700

    Pointers

---
+++ pointers/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -  compares two strings
+ *
+ * @s1: String
+ *
+ * @s2: String
+ *
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int n = strcmp(s1, s2);
+
+		return (n);
+}
+
+
