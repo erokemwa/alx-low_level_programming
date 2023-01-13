b1de642176661372b73383030f98d75b988591b9
erokemwa <erokemwa@yahoo.com>
2023-01-13 04:45:51 -0800

    table_delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,35 +1,30 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_delete - deletes a hash table
- *@ht: the hash table to be deleted
- *Return: void
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *current = NULL;
-	hash_node_t *nxt = NULL;
-	unsigned long int idx;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-	if (ht == NULL)
-		return;
-	for (idx = 0; idx < ht->size; idx++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[idx] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			current = ht->array[idx];
-			nxt = current;
-			while (nxt != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				nxt = nxt->next;
-				free(current->value);
-				free(current->key);
-				free(current);
-				current = nxt;
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
